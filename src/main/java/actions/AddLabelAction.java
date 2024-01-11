0f5a6538b12542367003e7665677bed7894ce762
TTangNingzhi <tnz241736@gmail.com>
2024-01-11 14:58:06 -0500

    javadoc

--- src/main/java/actions/AddLabelAction.java
+++ src/main/java/actions/AddLabelAction.java
@@ -7,6 +7,9 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class is the action for adding labels.
+ */
 public class AddLabelAction extends AnAction {
 
     private String description;
@@ -18,6 +21,11 @@ public class AddLabelAction extends AnAction {
         e.getPresentation().setEnabled(isEnabled);
     }
 
+    /**
+     * This method is called when the action is performed. It will show a notification to indicate that the label is successfully added.
+     *
+     * @param e The action event.
+     */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Notification notification = new Notification("CodeGRITS Notification Group", "Add label",
--- src/main/java/actions/ConfigAction.java
+++ src/main/java/actions/ConfigAction.java
@@ -4,11 +4,14 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
-import component.ConfigDialog;
+import components.ConfigDialog;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+/**
+ * This class is the action for configuring the application.
+ */
 public class ConfigAction extends AnAction {
 
     private static boolean isEnabled = true;
@@ -18,6 +21,11 @@ public class ConfigAction extends AnAction {
         e.getPresentation().setEnabled(isEnabled);
     }
 
+    /**
+     * This method is called when the action is performed. It will show the configuration dialog.
+     *
+     * @param e The action event.
+     */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
